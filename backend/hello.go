1d21a03eead1e4ea41d4eeac02efc43405722267
dsantos747 <d.santos74747@gmail.com>
2023-12-16 22:37:16 +0000

    Implemented first test cloud function

--- backend/hello.go
+++ backend/hello.go
@@ -1,15 +1,30 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"html"
 	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
- 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	var resp []byte = []byte(`HELLO MR WORLD HOW ARE YOU`)
-		
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Content-Length", fmt.Sprint(len(resp)))
-	w.Write(resp)
-  	// fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+func init() {
+	functions.HTTP("HelloHTTP", HelloHTTP)
+}
+
+// HelloHTTP is an HTTP Cloud Function with a request parameter.
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+			Name string `json:"name"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+			fmt.Fprint(w, "Hello, World!")
+			return
+	}
+	if d.Name == "" {
+			fmt.Fprint(w, "Hello, World!")
+			return
+	}
+	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
 }
