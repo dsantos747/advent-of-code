bdcec7cc061af8638e140bfc2226f05496db79c2
dsantos747 <d.santos74747@gmail.com>
2023-12-23 18:00:23 +0000

    Renamed day-1 package

--- backend/day-1/day-1.go
+++ backend/day-1/day-1.go
@@ -1,12 +1,11 @@
-package main
+package day1
 
 import (
 	"fmt"
 	"regexp"
 	"strconv"
-	"strings"
-
-	tools "github.com/dsantos747/advent-of-code-2023/tools"
+	// "strings"
+	// tools "github.com/dsantos747/advent-of-code-2023/tools"
 )
 
 func numFormat(word string) string {
@@ -16,9 +15,9 @@ func numFormat(word string) string {
 		"three": "3",
 		"four":  "4",
 		"five":  "5",
-		"six":  "6",
-		"seven":  "7",
-		"eight":  "8",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
 		"nine":  "9",
 	}
 
@@ -32,9 +31,9 @@ func part1(input []string) int {
 	var calibSum int = 0
 	re := regexp.MustCompile(`[0-9]`)
 
-	for _,line := range input {
-		matches := re.FindAll([]byte(line),-1)
-		calib,_ := strconv.Atoi(string(matches[0]) + string(matches[len(matches)-1]))
+	for _, line := range input {
+		matches := re.FindAll([]byte(line), -1)
+		calib, _ := strconv.Atoi(string(matches[0]) + string(matches[len(matches)-1]))
 		calibSum += calib
 	}
 
@@ -45,28 +44,30 @@ func part2(input []string) int {
 	var calibSum int = 0
 	re := regexp.MustCompile(`[0-9]|one|two|three|four|five|six|seven|eight|nine`)
 
-	for _,line := range input {
-		matches := re.FindAll([]byte(line),-1)
+	for _, line := range input {
+		matches := re.FindAll([]byte(line), -1)
 		first := numFormat(string(matches[0]))
 		last := numFormat(string(matches[len(matches)-1]))
-		calib,_ := strconv.Atoi(first + last)
+		calib, _ := strconv.Atoi(first + last)
 		calibSum += calib
 	}
 
 	return calibSum
 }
 
-func main() {
-	data,err := tools.ReadInput("./input.txt")
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
-	input := strings.Split(data, "\n")
+func Day1(input []string) (int, int) {
+	// data, err := tools.ReadInput("./input.txt")
+	// if err != nil {
+	// 	fmt.Println("Error reading input:", err)
+	// 	return 0,0
+	// }
+	// input := strings.Split(data, "\n")
 
 	p1 := part1(input)
-	fmt.Println("The answer to part 1 is",p1)
+	fmt.Println("The answer to part 1 is", p1)
 
 	p2 := part2(input)
-	fmt.Println("The answer to part 2 is",p2)
+	fmt.Println("The answer to part 2 is", p2)
+
+	return p1, p2
 }
